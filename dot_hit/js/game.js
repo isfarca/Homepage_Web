700494ef37616517c6c803185f6afd5a0e558ca7
Fethi Isfarca <fisfarca@googlemail.com>
2019-05-04 20:38:44 +0200

    New structure and Comments to game logic script from dot hit game.

--- dot_hit/js/game.js
+++ dot_hit/js/game.js
@@ -1,86 +1,182 @@
-gamelength=30;
-timerID=null;
-var playing=false;
-var numholes=6*10;
-var currentpos=-1;
-function clrholes() {
-    for(var k=0;k<document.dmz.elements.length;k++)
-        document.dmz.elements[k].checked=false;
+// Default game settings.
+gamelength = 30;
+timerID = null;
+
+// Default values.
+let playing = false;
+let numberHoles = 6 * 10;
+let currentPosition = -1;
+
+// Clear holes.
+function clearHoles()
+{
+    // Set unchecked value to all radio buttons.
+    for (let i = 0; i < document.playPanel.elements.length; i++)
+    {
+        document.playPanel.elements[i].checked = false;
+    }
 }
-function stoptimer() {
-    if(playing)
+
+// Stop timer.
+function stopTimer()
+{
+    // Are you playing?
+    if (playing) // Yes.
+    {
+        // Clear timer.
         clearTimeout(timerID);
+    }
 }
-function showtime(remtime) {
-    document.cpanel.timeleft.value=remtime;
-    if(playing) {
-        if(remtime==0) {
-            stopgame();
-            return;
+
+// Output timer for player.
+function showTimer(rememberTime)
+{
+    // Write timer to field.
+    document.controlPanel.timeleft.value = rememberTime;
+
+    // Are you playing?
+    if (playing) // Yes.
+    {
+        // No time passed?
+        if (rememberTime === 0) // Yes.
+        {
+            // Stop game.
+            stopGame();
         }
-        else {
-            temp=remtime-1;
-            timerID=setTimeout("showtime(temp)",1000);
+        else // No.
+        {
+            // Decrement time, so that it passes.
+            let tempTime = rememberTime - 1;
+
+            // Call the timer function every second.
+            setTimeout
+            (
+                showTimer,1000, tempTime
+            );
         }
     }
 }
-function stopgame() {
-    stoptimer();
-    playing=false;
-    document.cpanel.timeleft.value=0;
-    clrholes();
+
+// Stop game.
+function stopGame()
+{
+    // Stop timer.
+    stopTimer();
+
+    // Get all total hits.
+    let totalHits = document.controlPanel.timeleft.value;
+
+    // Change default values for player.
+    playing = false;
+    document.controlPanel.timeleft.value = 0;
+
+    // Clear holes and output game over in the field.
+    clearHoles();
     display("Game Over");
-    alert('Game Over.\nYour score is:  '+totalhits);
+
+    // Show message with total hits information.
+    alert("Game Over.\nYour score is: " + totalHits);
 }
-function play() {
-    stoptimer();
-    if(playing) {
-        stopgame();
+
+// Start game.
+function play()
+{
+    // First of all, to stop timer, when is it running.
+    stopTimer();
+
+    // Are you playing?
+    if (playing)
+    {
+        // Stop game and exit this function (toggle effect).
+        stopGame();
+
         return;
     }
-    playing=true;
-    clrholes();
-    totalhits=0;
-    document.cpanel.score.value=totalhits;
+
+    // Change player status.
+    playing = true;
+
+    // Write default values for starting game.
+    clearHoles();
+    document.controlPanel.score.value = 0;
     display("Playing");
+
+    // Start the game and timer.
     launch();
-    showtime(gamelength);
+    showTimer(gamelength);
 }
-function display(msg) {
-    document.cpanel.state.value=msg;
+
+// Output text to field.
+function display(message)
+{
+    document.controlPanel.state.value = message;
 }
-function launch() {
-    var launched=false;
-    while(!launched) {
-        mynum=random();
-        if(mynum!=currentpos) {
-            document.dmz.elements[mynum].checked=true;
-            currentpos=mynum;
-            launched=true;
+
+// Game running.
+function launch()
+{
+    // Change launch status.
+    let launched = false;
+
+    // By not launching.
+    while (!launched)
+    {
+        // Get random radio button
+        let randomPositionNumber = random();
+
+        // Radio button id equal not the current radio button id.
+        if (randomPositionNumber !== currentPosition) // Yes.
+        {
+            // Checked status.
+            document.playPanel.elements[randomPositionNumber].checked = true;
+
+            // Write new current radio button id.
+            currentPosition = randomPositionNumber;
+
+            // Is launched status.
+            launched = true;
         }
     }
 }
 
-function hithead(id) {
-    if(playing==false) {
-        clrholes();
+// Hit radio button evaluation
+function hitHead(id)
+{
+    // Are you not playing?
+    if (!playing) // Yes.
+    {
+        // Clear holes and Output the task.
+        clearHoles();
         display("Push Start to Play");
+
+        // Exit this function.
         return;
     }
-    if(currentpos!=id) {
-        totalhits+=-1;
-        document.cpanel.score.value=totalhits;
-        document.dmz.elements[id].checked=false;
+
+    // Hit wrong button?
+    if (currentPosition !== id) // Yes.
+    {
+        // Decrement score.
+        document.controlPanel.score.value += -1;
+
+        // Unchecked clicked radio button.
+        document.playPanel.elements[id].checked = false;
     }
-    else {
-        totalhits+=1;
-        document.cpanel.score.value=totalhits;
+    else // No.
+    {
+        // Increment score.
+        document.controlPanel.score.value += 1;
+
+        // Refresh the radio button.
         launch();
-        document.dmz.elements[id].checked=false;
+
+        // Unchecked clicked radio button.
+        document.playPanel.elements[id].checked = false;
     }
 }
 
-function random() {
-    return(Math.floor(Math.random()*100%numholes));
+// Get random radio button id.
+function random()
+{
+    return (Math.floor(Math.random() * 100 % numberHoles));
 }
-// End -->
